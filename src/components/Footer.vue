2674e83e24a7031636707a7a82221aa5d8be328a
TheJeffRicardo <konkemalindi@gmail.com>
2023-01-13 15:31:48 +0200

    'portfolio'

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,33 @@
 <template>
-    <pre>Copyright &copy; 2022 by KonkeJeff RicardoMalindi
-        All Rights Reserved
-    </pre>
+    <div class="foot">
+        <pre>Copyright &copy; 2022 by KonkeJeff RicardoMalindi
+            All Rights Reserved
+        </pre>
+        <a href="https://github.com/TheJeffRicardo?tab=repositories"><i class="bi bi-github text-dark"></i></a>
+        <a href="https://www.linkedin.com/messaging/thread/new/"><i class="bi bi-linkedin text-dark"></i></a>
+        <a href="https://discord.com/channels/@me"><i class="bi bi-discord text-dark"></i></a>
+    </div>
 </template>
 <script>
 export default {
     name: 'footer'
 }
 </script>
-<style>
+<style scoped>
 pre {
     font-family: 'Fredoka One';
     text-align: center;
     font-size: 15px;
     overflow-y: hidden;
-
+}
+.foot i{
+    padding: 10px;
+    font-size: 20px;
+    box-shadow: 0 0 20px 10px rgba(0,0,0,0.05);
+    border-radius: 50%;
+    transition: 0.3s;
+}
+.foot i:hover{
+    background-color: aliceblue;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
         <nav>
             <i class="bi bi-house-door-fill"></i><router-link to="/">Home</router-link>
             <i class="bi bi-book-half"></i><router-link to="/about">About</router-link>
-            <i class="bi bi-book-half"></i><router-link to="/resume">Resume</router-link>
+            <i class="bi bi-collection-fill"></i><router-link to="/resume">Resume</router-link>
             <i class="bi bi-briefcase-fill"></i><router-link to="/project">Projects</router-link>
             <i class="bi bi-people-fill"></i><router-link to="/testimonial">Testimonials</router-link>
             <i class="bi bi-telephone-plus-fill"></i><router-link to="/contact">Contact</router-link> 
